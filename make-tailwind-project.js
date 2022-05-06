0118c99eb2fc74706c3aab1ae89cd445349d884b
alex-eliot <stevets2001@hotmail.com>
2022-05-06 12:53:53 +0300

    update script

--- make-tailwind-project.js
+++ make-tailwind-project.js
@@ -2,15 +2,15 @@
 
 const fs = require('fs');
 
-const package = require('./package.json');
+const pkg = require('./package.json');
 
 input = '@tailwind base;\n@tailwind components;\n@tailwind utilities;';
 
-package.scripts = {
+pkg.scripts = {
     build: 'npx tailwindcss -i ./src/input.css -o ./dist/output.css',
     run: 'npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch'
 };
 
-fs.writeFileSync('./package.json', JSON.stringify(package, undefined, '  '));
+fs.writeFileSync('./package.json', JSON.stringify(pkg, undefined, '  '));
 fs.mkdirSync('./src');
 fs.writeFileSync('./src/input.css', input);
--- make-tailwind-project.sh
+++ make-tailwind-project.sh
@@ -1,3 +1,3 @@
 npm install -D tailwindcss
 npx tailwindcss init
-wget -qO- https://raw.githubusercontent.com/alex-eliot/scripts/make-tailwind-project.js | node
+wget -qO- https://raw.githubusercontent.com/alex-eliot/scripts/master/make-tailwind-project.js | node
